8d06d4ff3175b029f851d91b9f5781a106c90fc6
Yuvraj <yuvrajshekhawatpilani@gmail.com>
2022-12-27 16:41:20 +0530

    js modified

--- Portfolio 1/portfolio.js
+++ Portfolio 1/portfolio.js
@@ -7,7 +7,36 @@ function opentab(tabname){
     }
 
     for(tabcontent of tabcontents){
-        tabcontents.classList.remove("active-tab")
+        tabcontent.classList.remove("active-tab");
     }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
+
+var sidemenu = document.getElementById("sidemenu")
+
+function openmenu(){
+        sidemenu.style.right = "0";
+    }
+    function closemenu(){
+        sidemenu.style.right = "-200px";
+    }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyySg2wLhxN9-5KPsohMRWpJmed0TDXFE5XR2AOaoUSZAVE8MXsHW9Marwj1IhIqT-w5w/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully!!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 3000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
